7dbd4fd99307dc91f0156f1c3978fa4d8094aa99
Yoni Samlan <yoni@thelevelup.com>
2014-08-01 11:09:46 -0400

    Update license to Apache 2.0

--- README.md
+++ README.md
@@ -10,6 +10,14 @@ and the documentation for the [Deeplink Auth Library](levelUpDeeplinkAuthLib) an
 By enabling LevelUp integrations, including through this SDK, you agree to LevelUp's
 [developer terms](https://www.thelevelup.com/developer-terms).
 
-# Copyright
+# License
+ 
+Copyright (C) 2014 SCVNGR, Inc. d/b/a LevelUp
 
-Copyright 2013-2014 SCVNGR, Inc., D.B.A. LevelUp. All rights reserved.
+Except as otherwise noted, the LevelUp SDK for Android is licensed under the Apache License, Version
+2.0 (http://www.apache.org/licenses/LICENSE-2.0.html).
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is
+distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+implied. See the License for the specific language governing permissions and limitations under the
+License.
--- levelUpCoreTestLib/src/main/java/com/scvngr/levelup/core/test/SupportLoaderTestCase.java
+++ levelUpCoreTestLib/src/main/java/com/scvngr/levelup/core/test/SupportLoaderTestCase.java
@@ -1,21 +1,22 @@
 /*
- * This is a copy of the LoaderTestCase from android. It was modified to use the support library
- * instead of the default implementation. Modifications Copyright 2013-2014 SCVNGR, Inc., D.B.A.
- * LevelUp. All rights reserved.
+ * This is a copy of the LoaderTestCase from Android. It was modified to use the support library
+ * instead of the default implementation. Modifications Copyright (C) 2014 SCVNGR, Inc. d/b/a
+ * LevelUp
  *
- * CHECKSTYLE:OFF Original license:
- * Copyright (C) 2010 The Android Open Source
- * Project
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License
+ * The original AOSP implementation is also Apache licensed; Copyright (C) 2010 The Android Open
+ * Source Project.
  */
 package com.scvngr.levelup.core.test;
 
